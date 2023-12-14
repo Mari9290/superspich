c264d758a8aca95aa6bef2dbee86f401650713f4
marina9290 <marina9290@yandex.ru>
2023-12-14 22:27:26 +0300

    police offerta link favicon title description

--- js/karusel-bilingual.js
+++ js/karusel-bilingual.js
@@ -1,7 +1,7 @@
 const next = document.querySelector('#nextB');
 const back = document.querySelector('#backB');
 
-const photos = ['img/reviews-bilingual/2.jpg', 'img/reviews-bilingual/3.jpg'];
+const photos = ['img/reviews-bilingual/2.jpg', 'img/reviews-bilingual/3.jpg', 'img/reviews-bilingual/4.jpeg', 'img/reviews-bilingual/5.jpeg', 'img/reviews-bilingual/6.jpg', 'img/reviews-bilingual/7.jpeg', 'img/reviews-bilingual/8.jpg'];
 
 let i = 0;
 next.addEventListener('click', () => {
